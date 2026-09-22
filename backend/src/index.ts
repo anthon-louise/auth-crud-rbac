7d99311610b27e24e4bbabfc68865331bb8a6c46
anthon-dev <anthonlouisenoynay123@example.com>
2026-09-22 19:18:36 +0800

    sql table

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
