d845ee152247fd13072b420804eb18200425c725
anthon-dev <anthonlouisenoynay123@example.com>
2026-09-25 07:37:07 +0800

    register page setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div>App</div>
+    <QueryClientProvider client={queryClient}>
+      <Toaster richColors position="top-right"/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<RegisterPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
 
